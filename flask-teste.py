f4c51f4f89efe3db9e3c31b9248b5c6807cf9990
Filipe André <flipeandre12@gmail.com>
2023-01-21 20:24:29 -0300

    Initial commit

---
+++ flask-teste.py
@@ -0,0 +1,34 @@
+import json
+import requests
+from flask import Flask
+from flask import render_template
+from flask import request
+app = Flask(__name__)
+
+
+@app.route('/')
+def pagina():
+    return render_template('pagina.html')
+
+
+@app.route('/pesquisar', methods=['POST'])
+def pesquisar():
+    filme = request.form.get('texto')
+    try:
+        req = requests.get(f'http://www.omdbapi.com/?t={filme}&apikey=9b11133e')
+    except:
+        return render_template('pagina.html')
+
+    filme = json.loads(req.text)
+    nome = filme['Title']
+    ano = filme['Year']
+    escritor = filme['Writer']
+    genero = filme['Genre']
+    ator = filme['Actors']
+    trama = filme['Plot']
+    poster = filme['Poster']
+    return render_template('lista.html', nome=nome, ano=ano, escritor=escritor, genero=genero, ator=ator, trama=trama,
+                           poster=poster)
+
+
+app.run(host='0.0.0.0', port=81)
